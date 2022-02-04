abd8c4725d1aff4e0212764859469a2f331042ec
TILLERN <brysonminodi@google.com>
2022-02-05 02:38:06 +0300

    api model implimentation

--- app/src/main/java/com/example/cryptocurrencyapp/presentation/theme/Shape.kt
+++ app/src/main/java/com/example/cryptocurrencyapp/presentation/theme/Shape.kt
@@ -1,4 +1,4 @@
-package com.example.cryptocurrencyapp.ui.theme
+package com.example.cryptocurrencyapp.presentation.theme
 
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Shapes
